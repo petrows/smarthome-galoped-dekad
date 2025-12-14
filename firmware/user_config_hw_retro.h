02e2c34c0921691df49212e96786c2eb11186f64
Petro <petro@petro.ws>
2025-12-14 19:07:59 +0100

    Add matrix to build rgb/retro versions in ci

---
+++ firmware/user_config_hw_retro.h
@@ -0,0 +1,7 @@
+/*
+  Part for Galoped-dekad hw version:
+
+    * Backlight: RGB
+*/
+
+#define USER_TEMPLATE "{\"NAME\":\"Galoped-dekad-retro\",\"GPIO\":[1,1,1,1,1,1,1,1,1,1,416,0,1600,1632,1,1,0,640,608,1,0,1,12160,12192,0,0,0,0,1,1,32,1,1,0,0,1],\"FLAG\":0,\"BASE\":1}"
---
+++ firmware/user_config_hw_rgb.h
@@ -0,0 +1,7 @@
+/*
+  Part for Galoped-dekad hw version:
+
+    * Backlight: RGB
+*/
+
+#define USER_TEMPLATE "{\"NAME\":\"Galoped-dekad-rgb\",\"GPIO\":[1,1,1,1,1,1,1,1,1,1,0,1376,1600,1632,1,1,0,640,608,1,0,1,12160,12192,0,0,0,0,1,1,32,1,1,0,0,1],\"FLAG\":0,\"BASE\":1}"
