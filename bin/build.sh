d763db35f74e62b058ead6498fc82caea6d5bf27
Petro <petro@petro.ws>
2025-12-14 23:21:01 +0100

    Decouple safeboot compile before main code

--- bin/build.sh
+++ bin/build.sh
@@ -12,15 +12,19 @@ echo "Building firmware for variant: $VARIANT"
 
 cd firmware/tasmota
 
+# Build safeboot binary (if CI)
+if [ "$CI" = "true" ]; then
+    rm -rf tasmota/user
+    rm -rf tasmota/user_config_override.h
+    platformio run -e tasmota32-safeboot
+fi
+
 # Copy config overrides
 cp ../user_config_override.h tasmota/
 
 # Copy HW version config
 mkdir -p tasmota/user/
 cp ../user_config_hw_${VARIANT}.h tasmota/user/user_config_hw.h
-
-# Build safeboot binary
-platformio run -e tasmota32-safeboot
 
 # Build firmware
 platformio run -e tasmota32
