2394b2f1e18320f347b516b011de337abb388235
Petro <petro@petro.ws>
2026-03-26 18:38:56 +0100

    Update Github actions workflow

--- bin/build.sh
+++ bin/build.sh
@@ -3,7 +3,7 @@
 CHIP=$1
 VARIANT=$2
 
-if [ -z "$CHIP" || -z "$VARIANT" ]; then
+if [[ -z "$CHIP" ]] || [[ -z "$VARIANT" ]]; then
     echo "Usage: $0 <chipset> <variant>"
     echo "Available chipset: esp32, esp32c6"
     echo "Available variants: rgb, retro, biax"
@@ -12,7 +12,9 @@ fi
 
 echo "Building firmware for cipset: $CHIP, variant: $VARIANT"
 
-cd firmware/tasmota
+FW_NAME=galoped-wifi-firmware-${CHIP}-${VARIANT}
+
+cd firmware-wifi/tasmota
 
 # Build safeboot binary (if CI)
 # if [ "$CI" = "true" ]; then
@@ -26,13 +28,13 @@ cp ../user_config_override.h tasmota/
 
 # Copy HW version config
 mkdir -p tasmota/user/
-cp ../user_config_${$CHIP}_${VARIANT}.h tasmota/user/user_config_hw.h
+cp ../user_config_${CHIP}-${VARIANT}.h tasmota/user/user_config_hw.h
 
 # Build firmware
 platformio run -e tasmota32
 
-cp .pio/build/tasmota32/firmware.bin ../galoped-wifi-firmware-${VARIANT}.bin
+cp .pio/build/tasmota32/firmware.bin ../${FW_NAME}.bin
 cd ..
-tar -czf galoped-wifi-firmware-${$CHIP}-${VARIANT}.tar.gz galoped-wifi-firmware-${${$CHIP}}-${VARIANT}.bin
+tar -czf ${FW_NAME}.tar.gz ${FW_NAME}.bin
 
-echo "Built firmware: firmware/galoped-wifi-firmware-${$CHIP}-${VARIANT}.tar.gz"
+echo "Built firmware: firmware-wifi/${FW_NAME}.tar.gz"
