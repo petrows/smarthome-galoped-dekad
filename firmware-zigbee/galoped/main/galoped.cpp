6307e05cfe0a013fbba22272f847e741f6ece609
Petro <petro@petro.ws>
2026-05-21 21:28:09 +0200

    Zigbee: cleanup old test app, new stub

---
+++ firmware-zigbee/galoped/main/galoped.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file galoped.cpp
+ * @author Petr Golovachev (petro@petro.ws)
+ * @brief Galoped Zigbee firmware entry point
+ *
+ */
+
+#include "esp_log.h"
+
+static const char *TAG = "GLP";
+
+extern "C" void app_main()
+{
+    ESP_LOGI(TAG, "App startup");
+}
